710f5850f8b534943e5b7b491a94f0ef328b2baf
developer-nayem <mdalimodassernayem@gmail.com>
2023-06-12 01:32:53 +0600

    dashboard menu use framer motion animation

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
 import logo from "../assets/logo/educations_logo.png";
 import {
    FaBookmark,
-   FaEdit,
    FaHome,
    FaPlus,
    FaRegBookmark,
@@ -58,53 +58,93 @@ const DashboardLayout = () => {
                />
             </div>
             <div className="pl-5">
-               <NavLink
-                  to="/"
-                  className="dash-nav-link"
+               <motion.p
+                  initial={{ opacity: 0, x: "-100%" }}
+                  whileInView={{ opacity: 1, x: 0 }}
+                  transition={{
+                     duration: 0.7,
+                  }}
+                  viewport={{ once: true }}
+                  whileTap={{ scale: 0.8 }}
                >
-                  <FaHome />
-                  Home
-               </NavLink>
+                  <NavLink
+                     to="/"
+                     className="dash-nav-link"
+                  >
+                     <FaHome />
+                     Home
+                  </NavLink>
+               </motion.p>
                {user?.role === "admin" ? (
                   <div>
                      {adminLink.map((link, i) => (
-                        <NavLink
-                           onClick={() => setHeadTitle(link.name)}
-                           to={`admin-dashboard/${link.path}`}
-                           key={i}
-                           className="dash-nav-link"
+                        <motion.p
+                           initial={{ opacity: 0, x: "-100%" }}
+                           whileInView={{ opacity: 1, x: 0 }}
+                           transition={{
+                              duration: i === 0 ? 1 : i + 0.5,
+                           }}
+                           viewport={{ once: true }}
+                           whileTap={{ scale: 0.8 }}
                         >
-                           {link.icon}
-                           {link.name}
-                        </NavLink>
+                           <NavLink
+                              onClick={() => setHeadTitle(link.name)}
+                              to={`admin-dashboard/${link.path}`}
+                              key={i}
+                              className="dash-nav-link"
+                           >
+                              {link.icon}
+                              {link.name}
+                           </NavLink>
+                        </motion.p>
                      ))}
                   </div>
                ) : user?.role === "instructor" ? (
                   <div>
                      {instructorLink.map((link, i) => (
-                        <NavLink
-                           onClick={() => setHeadTitle(link.name)}
-                           to={`instructor-dashboard/${link.path}`}
-                           key={i}
-                           className="dash-nav-link"
+                        <motion.p
+                           initial={{ opacity: 0, x: "-100%" }}
+                           whileInView={{ opacity: 1, x: 0 }}
+                           transition={{
+                              duration: i === 0 ? 1 : i + 0.5,
+                           }}
+                           viewport={{ once: true }}
+                           whileTap={{ scale: 0.8 }}
                         >
-                           {link.icon}
-                           {link.name}
-                        </NavLink>
+                           <NavLink
+                              onClick={() => setHeadTitle(link.name)}
+                              to={`instructor-dashboard/${link.path}`}
+                              key={i}
+                              className="dash-nav-link"
+                           >
+                              {link.icon}
+                              {link.name}
+                           </NavLink>
+                        </motion.p>
                      ))}
                   </div>
                ) : user?.role === "student" ? (
                   <div>
                      {studentLink.map((link, i) => (
-                        <NavLink
-                           onClick={() => setHeadTitle(link.name)}
-                           to={`student-dashboard/${link.path}`}
-                           key={i}
-                           className="dash-nav-link"
+                        <motion.p
+                           initial={{ opacity: 0, x: "-100%" }}
+                           whileInView={{ opacity: 1, x: 0 }}
+                           transition={{
+                              duration: i === 0 ? 1 : i + 0.5,
+                           }}
+                           viewport={{ once: true }}
+                           whileTap={{ scale: 0.8 }}
                         >
-                           {link.icon}
-                           {link.name}
-                        </NavLink>
+                           <NavLink
+                              onClick={() => setHeadTitle(link.name)}
+                              to={`student-dashboard/${link.path}`}
+                              key={i}
+                              className="dash-nav-link"
+                           >
+                              {link.icon}
+                              {link.name}
+                           </NavLink>
+                        </motion.p>
                      ))}
                   </div>
                ) : (
@@ -113,9 +153,17 @@ const DashboardLayout = () => {
             </div>
          </div>
          <div className="col-span-6">
-            <div className="text-2xl font-semibold capitalize p-4 px-8">
+            <motion.div
+               initial={{ opacity: 0, y: "-100%" }}
+               whileInView={{ opacity: 1, y: 0 }}
+               transition={{
+                  duration: 1,
+               }}
+               viewport={{ once: true }}
+               className="text-2xl capitalize font-semibold py-3 px-4"
+            >
                {headTitle ? headTitle : user && `${user?.role} Dashboard`}
-            </div>
+            </motion.div>
             <div className="bg-[#e7fdfe] min-h-screen p-5">
                <Outlet />
             </div>
--- src/routes/PrivateRouter.jsx
+++ src/routes/PrivateRouter.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../pages/Shared/Loading";
 
 const PrivateRoute = ({ children }) => {
    const location = useLocation();
    const { user, loading } = useContext(AuthContext);
    if (loading) {
-      return <h2 className="text-4xl font-semibold">Loading</h2>;
+      return <Loading />;
    } else {
       if (user) {
          return children;
